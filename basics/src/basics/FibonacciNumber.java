84c1083a545ee7fccdafc8f578fe2e44ded26565
Jeesub Lee <jeesub.brad.lee@gmail.com>
2020-12-15 11:14:45 -0800

    Fibonacci Number

---
+++ basics/src/basics/FibonacciNumber.java
@@ -0,0 +1,31 @@
+package basics;
+
+public class FibonacciNumber {
+
+	public static int getFibonacciNumber(int num) {
+		if (num < 2) {
+			return num;
+		}
+		int result = 0;
+		int[] acc = new int[] {0, 1};
+
+		for (int i = 2; i <= num; i++) {
+			result = acc[0] + acc[1];
+
+			if (i % 2 == 0) {
+				acc[0] = result;
+			} else {
+				acc[1] = result;
+			}
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 15; i++) {
+			System.out.print(getFibonacciNumber(i) + " ");
+		}
+	}
+
+}
