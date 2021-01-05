73d06f17e4d5efa363f92ead97aca1aa25a2d56e
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-01-04 16:23:16 -0800

    Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ leetcode/src/leetcode/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -0,0 +1,71 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 
+ * 1. Double loop
+ * 2. TreeMap
+ * 3. Double Deque
+ * 
+ * i = starts from 0
+ * j = from 0 to nums.length - 1
+ * 
+ * maxDeque: keeps max
+ * 	- while new input is bigger, removeLast
+ * 	- put new input into the Deque
+ * 	-> peekFirst will return the max value in the subarray
+ *
+ * minDeque: keeps min
+ * 	- while new input is smaller, removeLast
+ * 	- put new input into the Deque
+ * 	-> peekFirst will return the min value in the subarray
+ *
+ * if (max - min) > limit
+ * 	remove max or min, if the value equals to nums[i]
+ * 	i++
+ * 	-> remove nums[i] from the subarray
+ * 
+ * By doing this, we can keep the longest length
+ * Because we only need the length of the longest subarray,
+ * we don't need to keep the exact i and j
+ * @author jeesublee
+ *
+ */
+public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
+
+	public static int longestSubarray(int[] nums, int limit) {
+		Deque<Integer> maxDeque = new ArrayDeque<>();
+		Deque<Integer> minDeque = new ArrayDeque<>();
+		int i = 0;
+		for (int j = 0; j < nums.length; j++) {
+			while (!maxDeque.isEmpty() && maxDeque.peekLast() < nums[j]) {
+				maxDeque.removeLast();
+			}
+			while (!minDeque.isEmpty() && minDeque.peekLast() > nums[j]) {
+				minDeque.removeLast();
+			}
+			maxDeque.addLast(nums[j]);
+			minDeque.addLast(nums[j]);
+			if (maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
+				if (maxDeque.peekFirst() == nums[i]) {
+					maxDeque.removeFirst();
+				}
+				if (minDeque.peekFirst() == nums[i]) {
+					minDeque.removeFirst();
+				}
+				i++;
+			}
+		}
+		return nums.length - i;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {10,1,2,4,7,2};
+		int limit = 5;
+		System.out.print(longestSubarray(nums, limit));
+		// output: 4
+	}
+
+}
