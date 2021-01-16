ad56239d6d445a064e55614efd0abc65047efb2e
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-01-16 15:09:11 -0800

    Two Sum II - Input array is sorted

---
+++ leetcode/src/leetcode/TwoSumII.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * use two pointers
+ * one starts from the start, another one starts from the end
+ * if sum < target, left pointer++
+ * else if sum > target, right pointer++
+ * else, we found it
+ * @author jeesublee
+ *
+ */
+public class TwoSumII {
+
+	public static int[] twoSum(int[] numbers, int target) {
+		int left = 0;
+		int right = numbers.length - 1;
+		while (left < right) {
+			int sum = numbers[left] + numbers[right];
+			if (sum < target) {
+				left++;
+			} else if (sum > target) {
+				right--;
+			} else {
+				return new int[] { left + 1, right + 1 };
+			}
+		}
+		return new int[] { -1, -1 };
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = {2, 7, 11, 15};
+		int target = 9;
+		System.out.print(Arrays.toString(twoSum(numbers, target)));
+		// output: [1, 2]
+	}
+}
