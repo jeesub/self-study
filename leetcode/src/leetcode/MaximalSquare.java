f7deaf83d08aa700288dcd1e52651b9cea31e101
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-02-12 23:48:12 -0500

    Maximal Square

--- leetcode/src/leetcode/MaximalSquare.java
+++ leetcode/src/leetcode/MaximalSquare.java
@@ -12,16 +12,11 @@ package leetcode;
 public class MaximalSquare {
 
     public static int maximalSquare(char[][] matrix) {
-        int[][] dp = new int[matrix.length][matrix[0].length];
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
         int maxSide = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == '0') {
-                    continue;
-                }
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                    maxSide = Math.max(maxSide, dp[i][j]);
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                if (matrix[i - 1][j - 1] == '0') {
                     continue;
                 }
                 dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
