d0f5fc4687e422d9967e3cfefb800d5570962d1d
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-02-12 23:45:17 -0500

    Maximal Square

--- leetcode/src/leetcode/MaximalSquare.java
+++ leetcode/src/leetcode/MaximalSquare.java
@@ -1,47 +1,35 @@
 package leetcode;
 /**
- * 
- * DP table stores the maximum width of square include myself
- * DP[i][j] = if (matrix[i][j] == 1) ? min of left, top, left-top + 1 : 0
- * if DP[i][j] > maxNum ? update maxNum
- * return maxNum ^ 2
- *
- * matrix table
- * 1 0 1 0 0
- * 1 0 1 1 1
- * 1 1 1 1 1
- * 1 0 0 1 0
- * 
- * DP table
- * 1 0 1 0 0
- * 1 0 1 1 1 
- * 1 1 1 2 2 (min of (left: 2, top: 1, left-top: 1) + 1)
- * 1 0 0 1 0
- * 
- * @author jeesublee
- *
+ * Maximal Square.
+ * [DP]
+ * Make a DP table.
+ * dp[i][j] contains the valid longest side length.
+ * if matrix[i][j] == '1', dp[i][j] = Min of (dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+ * Keep a valid longest side length and update if possible while iterating.
+ * Return area after iteration.
+ * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
  */
 public class MaximalSquare {
 
-	public static int maximalSquare(char[][] matrix) {
-		int max = 0;
-		int[][] dp = new int[matrix.length][matrix[0].length];
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length; j++) {
-				if (matrix[i][j] == '0') {
-					continue;
-				}
-				if (i == 0 || j == 0) {
-					dp[i][j] = Character.getNumericValue(matrix[i][j]);
-					max = Math.max(max, dp[i][j]);
-					continue;
-				}
-				dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
-				max = Math.max(max, dp[i][j]);
-			}
-		}
-		return max * max;
-	}
+    public static int maximalSquare(char[][] matrix) {
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int maxSide = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                    continue;
+                }
+                dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                maxSide = Math.max(maxSide, dp[i][j]);
+            }
+        }
+        return maxSide * maxSide;
+    }
 
 	public static void main(String[] args) {
 		char[][] matrix = {
