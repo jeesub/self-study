52ad470da1b60a74fbb851592aca6d89d0e56629
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-02-17 19:23:33 -0500

    Search Insert Position

---
+++ leetcode/src/leetcode/SearchInsertPosition.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Search Insert Position.
+ * Binary search
+ * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
+ */
+public class SearchInsertPosition {
+
+    public static int searchInsert(int[] nums, int target) {
+        int index = Arrays.binarySearch(nums, target);
+        return index >= 0 ? index : Math.abs(index + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 8};
+        int target = 7;
+        System.out.println(searchInsert(nums, target));
+        // output: 3
+    }
+
+}
