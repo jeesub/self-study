445989a27be839c0d65c412d07a84dcdeb8283f2
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-03-22 22:46:25 -0400

    leetcode 49. Group Anagrams

---
+++ leetcode/src/leetcode/Q49_GroupAnagrams.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 49. Group Anagrams.
+ * Use HashMap to store anagrams.
+ * Sort characters in string and use it as a key.
+ * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
+ */
+public class Q49_GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String key = buildKey(str);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            result.add(entry.getValue());
+        }
+
+        return result;
+    }
+
+    private static String buildKey(String str) {
+        int[] arr = new int[26];
+        for (char c : str.toCharArray()) {
+            arr[c - 'a']++;
+        }
+
+        StringBuilder key = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                continue;
+            }
+            int n = arr[i];
+            while (n > 0) {
+                key.append((char) ('a' + i));
+                n--;
+            }
+        }
+        return key.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+        // output: [[eat, tea, ate], [bat], [tan, nat]]
+    }
+
+}
