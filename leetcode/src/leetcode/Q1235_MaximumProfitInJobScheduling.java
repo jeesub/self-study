7a39fa0c60694e8e40b8ba8a2363ed80ebb4d66e
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-03-22 22:10:17 -0400

    leetcode 1235. Maximum Profit in Job Scheduling

--- leetcode/src/leetcode/Q1235_MaximumProfitInJobScheduling.java
+++ leetcode/src/leetcode/Q1235_MaximumProfitInJobScheduling.java
@@ -1,20 +1,28 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 /**
  * Q1235. Maximum Profit in Job Scheduling.
- * [DP]
- * DP array keeps maximum profit at the time.
+ * 
+ * 1. DP
+ * DP array keeps the maximum profit at the time.
  * Offer jobs into min-heap by ascending order of end time.
  * Iterate through the time.
  * If peak value of min-heap == time, calculate new max profit.
  * Else, dp[i] = dp[i - 1]
+ * 
+ * 2. TreeSet
+ * Jobs in TreeSet keep the maximum profit at the end time.
+ * Sort jobs by end time.
+ * Add a job into TreeSet, updating the profit to the maximum profit.
  * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
  */
 public class Q1235_MaximumProfitInJobScheduling {
 
-    public static int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+    public static int jobSchedulingArray(int[] startTime, int[] endTime, int[] profit) {
         PriorityQueue<Job> jobs = new PriorityQueue<>();
         int lastTime = 0;
         for (int i = 0; i < startTime.length; i++) {
@@ -33,6 +41,27 @@ public class Q1235_MaximumProfitInJobScheduling {
         return dp[lastTime];
     }
 
+    public static int jobSchedulingTreeSet(int[] startTime, int[] endTime, int[] profit) {
+        Job[] jobs = new Job[startTime.length];
+        for (int i = 0; i < jobs.length; i++) {
+            jobs[i] = new Job(startTime[i], endTime[i], profit[i]);
+        }
+        Arrays.sort(jobs);
+
+        TreeSet<Job> treeSet = new TreeSet<>();
+        treeSet.add(new Job(0, 0, 0));
+        int maxProfit = 0;
+        for (int i = 0; i < jobs.length; i++) {
+            Job job = jobs[i];
+            int prevProfit = treeSet.floor(new Job(Integer.MIN_VALUE, job.start, Integer.MAX_VALUE)).profit;
+            job.profit += prevProfit;
+            maxProfit = Math.max(maxProfit, job.profit);
+            job.profit = maxProfit;
+            treeSet.add(job);
+        }
+        return maxProfit;
+    }
+
     private static class Job implements Comparable<Job> {
         int start;
         int end;
@@ -49,6 +78,9 @@ public class Q1235_MaximumProfitInJobScheduling {
             if (this.end != other.end) {
                 return Integer.compare(this.end, other.end);
             }
+            if (this.profit != other.profit) {
+                return Integer.compare(this.profit, other.profit);
+            }
             return Integer.compare(this.start, other.start);
         }
     }
@@ -57,7 +89,8 @@ public class Q1235_MaximumProfitInJobScheduling {
         int[] startTime = {1,2,3,4,6};
         int[] endTime = {3,5,10,6,9};
         int[] profit = {20,20,100,70,60};
-        System.out.println(jobScheduling(startTime, endTime, profit));
+        System.out.println(jobSchedulingArray(startTime, endTime, profit));
+        System.out.println(jobSchedulingTreeSet(startTime, endTime, profit));
         // output: 150
     }
 
