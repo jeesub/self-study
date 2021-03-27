d5e712c305900967486021092f140aa9e6a90daf
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-03-27 14:46:25 -0400

    leetcode 285. Inorder Successor in BST

---
+++ leetcode/src/leetcode/Q285_InorderSuccessorInBST.java
@@ -0,0 +1,44 @@
+package leetcode;
+/**
+ * Q285. Inorder Successor in BST.
+ * 1. Go to find p
+ *    while going down, if we need to go to the left, curr is a successor candidate.
+ * 2. After find p
+ *    If p has right child, we need to dig into it.
+ *    Get the most left deepest child of the left child. It is a successor.
+ * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
+ */
+public class Q285_InorderSuccessorInBST {
+
+    public static TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode successor = null;
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.val == p.val) {
+                break;
+            }
+            if (curr.val > p.val) {
+                successor = curr;
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        if (curr != null && curr.right != null) {
+            successor = curr.right;
+            while(successor.left != null) {
+                successor = successor.left;
+            }
+        }
+        return successor;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println(inorderSuccessor(root, root.left).val);
+        // output: 2
+    }
+
+}
---
+++ leetcode/src/leetcode/TreeNode.java
@@ -0,0 +1,18 @@
+package leetcode;
+/**
+ * TreeNode.
+ * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+        
+    }
+
+    public TreeNode(int newVal) {
+        val = newVal;
+    }
+}
