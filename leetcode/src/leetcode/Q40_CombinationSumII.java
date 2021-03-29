baa91982cbeef4925d191c44ae7f7a8ef990e2df
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-03-29 16:20:24 -0400

    leetcode 40. Combination Sum II

--- leetcode/src/leetcode/Q40_CombinationSumII.java
+++ leetcode/src/leetcode/Q40_CombinationSumII.java
@@ -7,24 +7,21 @@ import java.util.List;
 /**
  * Q40. Combination Sum II
  * [BTR]
- * Sort the candidates.
- * In iteration, we should see each number only once.
- * Starts btr(idx = 0).
- * We need to avoid duplicate cases.
- * If we've never seen a number, we should include it.
- * ex) [1, 2, 2, 2, 5]
- *            ^
- *    In btr(idx = 2), it's the first time to see this number.
- *    -> We should include this number.
- *    In btr(idx = 1) and btr(idx = 0), it's not the first time to see this number.
- *    -> We should not include this number because, it'll make a duplicate.
- * 
- *               ^
- *    In btr(idx = 2), we've already seen this number in btr(idx = 3).
+ * Sort the array and BTR.
+ * We should avoid duplicates.
+ * [1, 1, 1, 2]
+ *  o  o  o  is valid.
+ *  o  o  x  is valid.
+ *  o  x  x  is valid.
+ *  x  x  x  is valid.
+ *  o  x  o  is invalid.
+ *  x  o  o  is invalid.
+ *  In case we go without the current number, we should skip the next same numbers.
  * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
  */
 public class Q40_CombinationSumII {
 
+    // Solution 1
     public static List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
@@ -35,6 +32,7 @@ public class Q40_CombinationSumII {
         return result;
     }
 
+    // for loop BTR solution
     private static void btr(int[] candidates, List<Integer> path, List<List<Integer>> result, int target, int index) {
         if (target == 0) {
             result.add(new ArrayList<>(path));
@@ -42,6 +40,7 @@ public class Q40_CombinationSumII {
         }
         
         for (int i = index; i < candidates.length; i++) {
+            // pruning
             if (candidates[i] > target) {
                 break;
             }
@@ -54,10 +53,47 @@ public class Q40_CombinationSumII {
         }
     }
 
+    // Solution 2
+    public static List<List<Integer>> combinationSum2Recursion(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        btr2(candidates, path, result, target, 0);
+        
+        return result;
+    }
+
+    // recursion BTR solution
+    private static void btr2(int[] candidates, List<Integer> path, List<List<Integer>> result, int target, int index) {
+        if (target == 0) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        if (target < 0 || index >= candidates.length) {
+            return;
+        }
+
+        // pruning
+        if (candidates[index] > target) {
+            return;
+        }
+        path.add(candidates[index]);
+        btr(candidates, path, result, target - candidates[index], index + 1);
+        path.remove(path.size() -1);
+        
+        int nextIndex = index + 1;
+        while (nextIndex < candidates.length && candidates[index] == candidates[nextIndex]) {
+            nextIndex++;
+        }
+        btr(candidates, path, result, target, nextIndex);
+    } 
+
     public static void main(String[] args) {
         int[] candidates = {2, 1, 2, 5, 2};
         int target = 5;
         System.out.println(combinationSum2(candidates, target));
+        System.out.println(combinationSum2Recursion(candidates, target));
         // output: [[1, 2, 2], [5]]
     }
 
