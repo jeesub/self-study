c01116f80b2ffc7a96269488e2005eb3bb29bbfb
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-04-12 21:30:35 -0400

    leetcode 1448. Count Good Nodes in Binary Tree

---
+++ leetcode/src/leetcode/Q1448_CountGoodNodesInBinaryTree.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+public class Q1448_CountGoodNodesInBinaryTree {
+
+    private static int count = 0;
+
+    public static int goodNodes(TreeNode root) {
+        countGoodNodes(root, root.val);
+        return count;
+    }
+
+    private static void countGoodNodes(TreeNode node, int maxSoFar) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.val >= maxSoFar) {
+            count++;
+        }
+
+        maxSoFar = Math.max(maxSoFar, node.val);
+        countGoodNodes(node.left, maxSoFar);
+        countGoodNodes(node.right, maxSoFar);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(1);
+        root.left.left = new TreeNode(3);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(5);
+        System.out.println(goodNodes(root));
+        // output: 4
+    }
+
+}
