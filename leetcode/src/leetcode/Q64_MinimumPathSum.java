10806930838b6b9119bc54bfec4f8ada9b2d2434
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-04-14 12:51:22 -0400

    leetcode 64. Minimum Path Sum

--- leetcode/src/leetcode/Q64_MinimumPathSum.java
+++ leetcode/src/leetcode/Q64_MinimumPathSum.java
@@ -3,23 +3,22 @@ package leetcode;
 public class Q64_MinimumPathSum {
 
     public static int minPathSum(int[][] grid) {
-        int[][] dp = init(grid);
+        int[] dp = init(grid);
         for (int i = 1; i < grid.length; i++) {
-            for (int j = 1; j < grid[0].length; j++) {
-                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            for (int j = 0; j < grid[0].length; j++) {
+                int top = dp[j];
+                int left = j > 0 ? dp[j - 1] : Integer.MAX_VALUE;
+                dp[j] = grid[i][j] + Math.min(top, left);
             }
         }
-        return dp[dp.length - 1][dp[0].length - 1];
+        return dp[dp.length - 1];
     }
-
-    private static int[][] init(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
-        dp[0][0] = grid[0][0];
-        for (int i = 1; i < grid.length; i++) {
-            dp[i][0] = dp[i - 1][0] + grid[i][0];
-        }
+    
+    private static int[] init(int[][] grid) {
+        int[] dp = new int[grid[0].length];
+        dp[0] = grid[0][0];
         for (int i = 1; i < grid[0].length; i++) {
-            dp[0][i] = dp[0][i - 1] + grid[0][i];
+            dp[i] = dp[i - 1] + grid[0][i];
         }
         return dp;
     }
