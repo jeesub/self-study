92a1dc265c7b6adcd55ce9e299f79e35fa080fd2
Jeesub Lee <jeesub.brad.lee@gmail.com>
2021-04-21 13:48:15 -0400

    leetcode 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ leetcode/src/leetcode/Q105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,48 @@
+package leetcode;
+/**
+ * Q105. Construct Binary Tree from Preorder and Inorder Traversal.
+ * preorder = [3 || 9 || 20, 15, 7]
+ *             ^ root
+ *                  ^ left child (in indorder, it's on the left side of 3)
+ *                       ^ right children (it's on the right side of left deepest child)
+ * inorder =  [9 || 3 || 15, 20, 7]
+ *                  ^ root
+ *             ^ left deepest child (the first element of the inorder)
+ * @author Jeesub Lee (jeesubl@andrew.cmu.edu)
+ */
+public class Q105_ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        return buildTree(preorder, 0, inorder, 0, inorder.length - 1);
+    }
+
+    private static TreeNode buildTree(int[] pre, int preIdx, int[] in, int inStart, int inEnd) {
+        if (inStart > inEnd) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode(pre[preIdx]);
+
+        int inIdx = find(in, pre[preIdx]);
+        root.left = buildTree(pre, preIdx + 1, in, inStart, inIdx - 1);
+        root.right = buildTree(pre, preIdx + inIdx - inStart + 1, in, inIdx + 1, inEnd);
+
+        return root;
+    }
+
+    private static int find(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        throw new RuntimeException("Wrong preorder and inorder");
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        System.out.println(buildTree(preorder, inorder));
+    }
+
+}
--- leetcode/src/leetcode/TreeNode.java
+++ leetcode/src/leetcode/TreeNode.java
@@ -15,4 +15,39 @@ public class TreeNode {
     public TreeNode(int newVal) {
         val = newVal;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("preorder: [");
+        preorderTraverse(this, sb);
+        sb.setLength(sb.length() - 2);
+        sb.append("] , inorder: [");
+        inorderTraverse(this, sb);
+        sb.setLength(sb.length() - 2);
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private void preorderTraverse(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        sb.append(node.val);
+        sb.append(", ");
+        preorderTraverse(node.left, sb);
+        preorderTraverse(node.right, sb);
+    }
+
+    private void inorderTraverse(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        inorderTraverse(node.left, sb);
+        sb.append(node.val);
+        sb.append(", ");
+        inorderTraverse(node.right, sb);
+    }
 }
